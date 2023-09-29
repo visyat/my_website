bb349ff97f94a96b623382bd172ceec3fe928bb2
Vishal Yathish <135551666+visyat@users.noreply.github.com>
2023-09-29 16:38:14 -0700

    Add files via upload

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home (request): 
+    #return HttpResponse ('Hello. Welcome to my very first VERY UGLY website using Django LMAO - Vishal :D')
+    return render(request, 'home.html')
